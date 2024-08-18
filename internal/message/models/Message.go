9d12453219306efffc0fabefd59db921fcf133e1
Ecl1ps0 <93914852+Ecl1ps0@users.noreply.github.com>
2024-08-19 02:05:39 +0500

    feat: message and chat models

---
+++ internal/message/models/Message.go
@@ -0,0 +1,12 @@
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Message struct {
+	ID        primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	Message   string             `json:"message" bson:"message"`
+	UserFrom  string             `json:"user_from" bson:"user_from"`
+	CreatedAt int64              `json:"created_at" bson:"created_at"`
+}
