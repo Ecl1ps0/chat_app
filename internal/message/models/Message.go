ff90ae918fda9ec6b8026955fdf223fe1d069c0a
Ecl1ps0 <93914852+Ecl1ps0@users.noreply.github.com>
2024-08-27 19:14:18 +0500

    fix: UserFrom field data type

--- internal/message/models/Message.go
+++ internal/message/models/Message.go
@@ -7,6 +7,6 @@ import (
 type Message struct {
 	ID        primitive.ObjectID `json:"id" bson:"_id,omitempty"`
 	Message   string             `json:"message" bson:"message"`
-	UserFrom  string             `json:"user_from" bson:"user_from"`
+	UserFrom  primitive.ObjectID `json:"user_from" bson:"user_from"`
 	CreatedAt int64              `json:"created_at" bson:"created_at"`
 }
