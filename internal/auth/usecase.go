e31bb4c18407b5a8da6566c83880792873f38582
Ecl1ps0 <93914852+Ecl1ps0@users.noreply.github.com>
2024-10-08 18:54:23 +0500

    refactor: remove userDTO from return

--- internal/auth/usecase.go
+++ internal/auth/usecase.go
@@ -6,7 +6,7 @@ import (
 )
 
 type Usecase interface {
-	SignUp(ctx context.Context, user models.User) (models.UserDTO, error)
+	SignUp(ctx context.Context, user models.User) error
 	SignIn(ctx context.Context, username, password string) (string, error)
 	ParseToken(ctx context.Context, token string) (models.UserDTO, error)
 }
