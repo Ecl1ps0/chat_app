f2db8cd0d44d0f12c4e527c673f78cc5d63a13eb
Ecl1ps0 <93914852+Ecl1ps0@users.noreply.github.com>
2024-10-12 19:02:29 +0500

    refactor: move token generation logic to util

--- internal/auth/usecase.go
+++ internal/auth/usecase.go
@@ -8,5 +8,4 @@ import (
 type Usecase interface {
 	SignUp(ctx context.Context, user models.User) error
 	SignIn(ctx context.Context, username, password string) (string, error)
-	ParseToken(ctx context.Context, token string) (models.UserDTO, error)
 }
