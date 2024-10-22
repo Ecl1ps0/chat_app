937f1cf75c2c0502d0e34b72ae2f8d2e352f2436
Ecl1ps0 <93914852+Ecl1ps0@users.noreply.github.com>
2024-10-23 01:01:13 +0500

    feat: add images to models

--- internal/message/models/MessageDTO.go
+++ internal/message/models/MessageDTO.go
@@ -1,6 +1,7 @@
 package models
 
 type MessageDTO struct {
-	SenderID string `json:"sender_id"`
-	Message  string `json:"message"`
+	SenderID string   `json:"sender_id"`
+	Message  string   `json:"message,omitempty"`
+	Images   []string `json:"images,omitempty"`
 }
