16bd386ca41444bd5789e6df407bffc0cad1b8ec
Ecl1ps0 <93914852+Ecl1ps0@users.noreply.github.com>
2024-10-30 20:29:31 +0500

    feat: add more fields

--- internal/auth/models/UserDTO.go
+++ internal/auth/models/UserDTO.go
@@ -3,6 +3,9 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type UserDTO struct {
-	ID       primitive.ObjectID `json:"id" bson:"_id,omitempty"`
-	Username string             `json:"username" bson:"username"`
+	ID             primitive.ObjectID  `json:"id" bson:"_id,omitempty"`
+	Username       string              `json:"username" bson:"username"`
+	ProfilePicture *primitive.ObjectID `json:"profile_picture" bson:"profile_picture"`
+	Bio            *string             `json:"bio" bson:"bio"`
+	Email          *string             `json:"email" bson:"email"`
 }
