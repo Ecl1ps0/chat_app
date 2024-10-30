283c9c7fa9e651a21e5bde15facb48a41599c976
Ecl1ps0 <93914852+Ecl1ps0@users.noreply.github.com>
2024-10-30 20:33:23 +0500

    feat: add endpoints to router

--- internal/user/router.go
+++ internal/user/router.go
@@ -7,4 +7,6 @@ import (
 
 func (h *UserHandler) UserRouterInit(router *http.ServeMux, authHandler *auth.AuthHandler) {
 	router.HandleFunc("/api/user/available-users", authHandler.AuthMiddleware(h.GetAllAvailableUsers))
+	router.HandleFunc("/api/user", authHandler.AuthMiddleware(h.GetUser))
+	router.HandleFunc("/api/update/user", authHandler.AuthMiddleware(h.UpdateUser))
 }
