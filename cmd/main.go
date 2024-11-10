46599b296e775a6e043f497f8f0bd6d2ac06098c
Ecl1ps0 <93914852+Ecl1ps0@users.noreply.github.com>
2024-11-10 16:46:35 +0500

    feat|refactor: add message handling and pass only middleware instead full auth handler

--- cmd/main.go
+++ cmd/main.go
@@ -10,6 +10,7 @@ import (
 	"ChatApp/internal/image"
 	repository5 "ChatApp/internal/image/repository"
 	usecase5 "ChatApp/internal/image/usecase"
+	"ChatApp/internal/message"
 	repository3 "ChatApp/internal/message/repository"
 	usecase3 "ChatApp/internal/message/usecase"
 	"ChatApp/internal/user"
@@ -18,7 +19,6 @@ import (
 	"ChatApp/pkg/mongo"
 	server2 "ChatApp/pkg/server"
 	"context"
-	"github.com/joho/godotenv"
 	"github.com/rs/cors"
 	"log"
 	"net/http"
@@ -28,10 +28,6 @@ import (
 )
 
 func main() {
-	if err := godotenv.Load("./.env"); err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
 	db, err := mongo.InitDB(os.Getenv("MONGO_URI"))
 	if err != nil {
 		log.Fatalf("fail to connect database: %s", err.Error())
@@ -58,8 +54,11 @@ func main() {
 	chatHandler := chat.NewChatHandler(chatUc, messageUc, imageUc)
 	chatHandler.ChatRouterInit(router)
 
+	messageHandler := message.NewMessageHandler(messageUc)
+	messageHandler.MessageRouterInit(router, authHandler.AuthMiddleware)
+
 	userHandler := user.NewUserHandler(userUc, imageUc)
-	userHandler.UserRouterInit(router, authHandler)
+	userHandler.UserRouterInit(router, authHandler.AuthMiddleware)
 
 	imageHandler := image.NewImageHandler(usecase5.NewImageUsecase(imageRepo))
 	imageHandler.ImageRouterInit(router)
