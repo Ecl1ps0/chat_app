4e9d1db60e663aa1007d223c7a8779e73eafcbc2
Ecl1ps0 <93914852+Ecl1ps0@users.noreply.github.com>
2024-11-10 16:39:54 +0500

    refactor: save messages to chat as message id instead full object

--- internal/chat/models/Chat.go
+++ internal/chat/models/Chat.go
@@ -1,13 +1,12 @@
 package models
 
 import (
-	models2 "ChatApp/internal/message/models"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type Chat struct {
 	ID        primitive.ObjectID   `json:"id" bson:"_id,omitempty"`
 	Members   []primitive.ObjectID `json:"members" bson:"members"`
-	Messages  []models2.Message    `json:"messages" bson:"messages"`
+	Messages  []primitive.ObjectID `json:"messages" bson:"messages"`
 	CreatedAt int64                `json:"created_at" bson:"created_at"`
 }
