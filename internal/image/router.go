95760273787d50b83726dbc69ea74949b0dbdcec
Ecl1ps0 <93914852+Ecl1ps0@users.noreply.github.com>
2024-11-18 01:31:08 +0500

    refactor: change endpoint

--- internal/image/router.go
+++ internal/image/router.go
@@ -6,5 +6,5 @@ import (
 
 func (h *ImageHandler) ImageRouterInit(router *http.ServeMux, authMiddleware func(http.HandlerFunc) http.HandlerFunc) {
 	router.HandleFunc("/api/image", h.GetImage)
-	router.HandleFunc("/api/image/create", authMiddleware(h.UploadImages))
+	router.HandleFunc("/api/image/upload", authMiddleware(h.UploadImages))
 }
