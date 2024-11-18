66dfabbb6de92a7922322eeb1b6d8512a7580fb0
Ecl1ps0 <93914852+Ecl1ps0@users.noreply.github.com>
2024-11-18 18:03:28 +0500

    feat: add delete for fields

--- internal/message/models/MessageDTO.go
+++ internal/message/models/MessageDTO.go
@@ -3,10 +3,11 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type MessageDTO struct {
-	ID       primitive.ObjectID   `json:"id,omitempty" bson:"_id,omitempty"`
-	SenderID primitive.ObjectID   `json:"sender_id" bson:"_id"`
-	Message  string               `json:"message,omitempty"`
-	Images   []primitive.ObjectID `json:"images,omitempty"`
-	Audio    primitive.ObjectID   `json:"audio,omitempty"`
-	IsUpdate bool                 `json:"is_update,omitempty"`
+	ID        primitive.ObjectID   `json:"id,omitempty" bson:"_id,omitempty"`
+	SenderID  primitive.ObjectID   `json:"sender_id" bson:"_id"`
+	Message   string               `json:"message,omitempty"`
+	Images    []primitive.ObjectID `json:"images,omitempty"`
+	Audio     *primitive.ObjectID  `json:"audio,omitempty"`
+	DeleteFor []primitive.ObjectID `json:"delete_for,omitempty"`
+	IsUpdate  bool                 `json:"is_update,omitempty"`
 }
